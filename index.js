26798a6e5b18b1577c4c9a9c60009f37f5eb8822
Sai Teja <saiteja.g1801@gmail.com>
2024-03-16 13:25:22 +0530

    routes updated

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const app = express();
 
 app.use(express.json());
 
-// Create Routes
-app.use("/user/signup", signup);
+const router = express.Router();
 
-app.use("/user", UserAuth, userController);
+router.post("/user/signup", signup);
+
+router.post("/user", UserAuth, userController);
+
 
 // Handle wrong routes
 app.all("*", (req, res, next) => {
